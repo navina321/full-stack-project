012920e3cf95ad3c2be708e317666ce4bcfdf1b5
navina321 <81630548+navina321@users.noreply.github.com>
2023-03-23 13:12:17 +0000

    made header component

--- disney-attractions-front-end/src/App.jsx
+++ disney-attractions-front-end/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.scss';
-import Navigation from './components/Navigation/Navigation';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
+      <Header/>
     </div>
   );
 }
---
+++ disney-attractions-front-end/src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navigation from "../../components/Navigation/Navigation";
+import DisneyCastle from "../../assets/images/header-images/disney-castle.png";
+import DisneyLogo from "../../assets/images/header-images/disney-logo.png";
+import "./Header.scss";
+const Header = () => {
+  return (
+    <div className="header-component">
+      <div className="header-logo-container">
+        <img
+          src={DisneyCastle}
+          alt=" disney-castle-logo"
+          className="header-logo-container__logo"
+        />
+        <img
+          src={DisneyLogo}
+          alt="disney-title"
+          className="header-logo-container__title"
+        />
+      </div>
+      <div className="header-navigation">
+        <Navigation />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
