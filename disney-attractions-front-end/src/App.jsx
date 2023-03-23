4dc0c208008b4d8cdf78a6fcd658c0b5bf97bf0e
navina321 <81630548+navina321@users.noreply.github.com>
2023-03-23 22:29:03 +0000

    changed header and footer dividers from px to vh

--- disney-attractions-front-end/src/App.jsx
+++ disney-attractions-front-end/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.scss';
-import MainPageAttrCards from './components/MainPageAttrCards/MainPageAttrCards';
 import Header from './containers/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <MainPageAttrCards/>
     </div>
   );
 }
