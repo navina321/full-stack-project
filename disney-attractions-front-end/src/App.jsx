16c29c81941b35baf5f42591ddaf24efe10d9ad1
navina321 <81630548+navina321@users.noreply.github.com>
2023-03-23 12:50:13 +0000

    made Navigation component

--- disney-attractions-front-end/src/App.jsx
+++ disney-attractions-front-end/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.scss';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation/>
     </div>
   );
 }
---
+++ disney-attractions-front-end/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navigation.scss'
+
+const Navigation = () => {
+  return (
+    <div className = 'navigation-component'>
+        <h2 className='navigation-item'>Parks</h2>
+        <h2 className='navigation-item'>Characters</h2>
+        <h2 className='navigation-item'>Parades & Shows</h2>
+    </div>
+  )
+}
+
+export default Navigation
