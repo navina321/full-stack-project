5d62d1ca5af8dcfa071aa38dd7dfeb98195e8b5b
navina321 <81630548+navina321@users.noreply.github.com>
2023-03-23 20:36:10 +0000

    added dropdown to navigation, added styling

--- disney-attractions-front-end/src/components/Navigation/Navigation.jsx
+++ disney-attractions-front-end/src/components/Navigation/Navigation.jsx
@@ -2,11 +2,32 @@ import React from 'react'
 import './Navigation.scss'
 
 const Navigation = () => {
+
+
   return (
+    // <div className = 'navigation-component'>
+    //     <h2 className='navigation-item'>Parks</h2>
+    //     <h2 className='navigation-item'>Characters</h2>
+    //     <h2 className='navigation-item'>Parades & Shows</h2>
+    // </div>
     <div className = 'navigation-component'>
-        <h2 className='navigation-item'>Parks</h2>
-        <h2 className='navigation-item'>Characters</h2>
-        <h2 className='navigation-item'>Parades & Shows</h2>
+      <div className='navigation-dropdown'>
+        <button className='dropdown-button'>
+          Disney Parks
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div className='dropdown-items'>
+          <a href='#'>Magic Kingdom</a>
+          <a href='#'>Animal Kingdom</a>
+          <a href='#'>Hollywood Studios</a>
+          <a href='#'>EPCOT</a>
+          <a href='#'>Blizzard Beach</a>
+          <a href='#'>Typhoon Lagoon</a>
+          <a href='#'>Disney Springs</a>
+        </div>
+      </div>
+      <a href='#'>Characters</a>
+      <a href='#'>Parades & Shows</a>
     </div>
   )
 }
