8ada78e6b2e9f4a99fa34ce444b4555cbedd04b5
navina321 <81630548+navina321@users.noreply.github.com>
2023-03-30 13:07:11 +0100

    added fetch request to app

--- disney-attractions-front-end/src/App.jsx
+++ disney-attractions-front-end/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.scss';
+import { useState } from 'react';
 import ParkSummaryCard from './components/ParkSummaryCard/ParkSummaryCard';
 import Header from './containers/Header/Header';
 
 
 function App() {
+
+  const [attractions, setAttractions] = useState()
+
+  const url = `http://localhost:8080/attractions`;
+
+  const getAttractions = async() => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setAttractions(data)
+  }
+
+
+
   return (
     <div className="App">
      <Header/>
