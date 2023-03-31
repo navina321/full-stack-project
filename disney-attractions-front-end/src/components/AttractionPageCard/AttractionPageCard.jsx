792d9b087069054035b62e8e3bf1e4bfa69df6b5
navina321 <81630548+navina321@users.noreply.github.com>
2023-03-31 16:07:57 +0100

    connected attraction to main attraction page

--- disney-attractions-front-end/src/components/AttractionPageCard/AttractionPageCard.jsx
+++ disney-attractions-front-end/src/components/AttractionPageCard/AttractionPageCard.jsx
@@ -8,21 +8,21 @@ const AttractionPageCard = ({imageURL, attractionName, park, location, rideType,
     <div className='attraction-page-card'>
       <section className='attraction-card-top'>
         <div className='attraction-card-top__image-container'>
-          <img src={imageURL} alt = 'image' className='attraction-card-top__image'/>
+          <img src={'https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/630/354/75/dam/wdpro-assets/gallery/attractions/epcot/mission-space-advanced-training-lab/mission-space-advanced-training-lab-gallery00.jpg?1660918020687'} alt = 'image' className='attraction-card-top__image'/>
         </div>
         <div className='attraction-card-top__info'>
-        <h1 className="attraction-card-top__name attr-info">{attractionName}</h1>
+        <h1 className="attraction-card-top__name attr-info">{"Advanced Training Lab"}</h1>
           <div className="attraction-card-top__parks attr-info">
-            <h2 className="attraction-card-top__parks--park attr-info">{park}</h2>
-            <h3 className="attraction-card-top__parks--location attr-info"> - {location}</h3>
+            <h2 className="attraction-card-top__parks--park attr-info">{"EPCOT"}</h2>
+            <h3 className="attraction-card-top__parks--location attr-info"> - {"World Discovery"}</h3>
           </div>
-          <h3 className="attraction-card-top__ride attr-info">Ride Type: {rideType}</h3>
-          <h3 className="attraction-card-top__theme attr-info">Theme: {theme}</h3>
-          <h3 className="attraction-card-top__height attr-info">Height Limit: {height}</h3>
-          <h3 className="attraction-card-top__access attr-info">Accessibility: {access}</h3>
+          <h3 className="attraction-card-top__ride attr-info">Ride Type: {"Interactive"}</h3>
+          <h3 className="attraction-card-top__theme attr-info">Theme: {"Indoors"}</h3>
+          <h3 className="attraction-card-top__height attr-info">Height Limit: {"Any Height"}</h3>
+          <h3 className="attraction-card-top__access attr-info">Accessibility: {'yes'}</h3>
         </div>
       </section>
-      <p className='attraction-card-bottom'>{description}</p>
+      <p className='attraction-card-bottom'>{"Calling All Space CadetsGet ready for your next space mission by virtually “flying” over the surface of Mars in a jetpack. Race against other players to send a rocket into space. Make and send video postcards from the very edge of space.The smallest space explorers will be inspired to reach for the stars as they crawl through wormholes in a fun interactive playground, while older cadets play digital games that prep them for future missions."}</p>
     </div>
   )
 }
--- disney-attractions-front-end/src/pages/Homepage/Homepage.jsx
+++ disney-attractions-front-end/src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Header from '../../containers/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import GeneralParksCard from '../../components/GeneralParksCard/GeneralParksCard'
@@ -20,19 +21,27 @@ const Homepage = () => {
         <section className='home-page-container'>
             <h1 className='home-page-title'>Parks at Walt Disney World </h1>
             <div className='homepage-parks-card'>
+              <Link to = '/magic-kingdom' className='home-page-link'>
                 <GeneralParksCard image = {MagicKingdom} alt ='magic-kingdom' title = 'Magic Kingdom' description="The world's most famous theme park, Magic Kingdom Park at Walt Disney World Resort in Orlando is the place where childhood dreams come true."/>
-                
+              </Link>  
+              <Link to = '/animal-kingdom' className='home-page-link'>
                 <GeneralParksCard image = {AnimalKingdom} alt ='animal-kingdom' title = 'Animal Kingdom' description="Disney's Animal Kingdom is a park dedicated to the natural wonder and exotic wildlife of the earth."/>
-                
+              </Link>
+              <Link to = '/hollywood-studios' className='home-page-link'>
                 <GeneralParksCard image = {HollywoodStudios} alt ='hollywood-studios' title = 'Hollywood Studios' description="Hollywood Studios  is dedicated to the world of movies, television, and entertainment, and its attractions are based on some of the most popular movies and TV shows of all time."/>
-                
+              </Link>  
+              <Link to = '/epcot' className='home-page-link'>
                 <GeneralParksCard image = {EPCOT} alt ='epcot' title = 'EPCOT' description="EPCOT  is dedicated to the celebration of human achievement, innovation, and international culture, and its attractions are divided into two main areas: Future World and World Showcase."/>
-                
+              </Link>
+              <Link to = '/blizzard-beach' className='home-page-link'>
                 <GeneralParksCard image = {BlizzardBeach} alt ='blizzard-beach' title = 'Blizzard Beach' description="Blizzard Beach is a water park, which is designed to resemble a ski resort that has been melted by the Florida sun, creating a unique and exciting atmosphere."/>
-                
+              </Link> 
+              <Link to = '/typhoon-lagoon' className='home-page-link'>
                 <GeneralParksCard image = {TyphoonLagoon} alt ='typhoon-lagoon' title = 'Typhoon Lagoon' description="Typhoon Lagoon is a water park designed to resemble a tropical paradise that has been hit by a massive typhoon, creating a unique and exciting atmosphere."/>
-                
+              </Link> 
+              <Link to = '/disney-springs' className='home-page-link'>
                 <GeneralParksCard image = {DisneySprings} alt ='disney-springs' title = 'Disney Springs' description="Disney Springs is a shopping, dining, and entertainment district located in Walt Disney World Resort."/>
+              </Link>
             </div>
         </section>
         <Footer/>
